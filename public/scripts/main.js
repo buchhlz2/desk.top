9183c35783e6add772a9621760be34f60c68e364
Daniel Buchholz <buchhlz2@gmail.com>
2020-08-24 19:08:25 -0500

    Redefined some basics element classes; added JQueryUI for drag/drop...to be implemented

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,10 +1,11 @@
 $(document).ready(function(){
-    var $folderArea = $('<div class="col-sm text-center folder-container"><div class="folder"></div></div>');
+    let $folderArea = (row, index) => $(`<div class="col-lg text-center folder-container"><div class="folder" id="folder-${row}${index}"></div></div>`);
+
     function addFolderDesktop(i, j) {
         for(let i = 0; i < 8; i++) {
             $('#desktop').append(`<div class="row text-center" id="desktop-row-${i}"></div>`)
-            for(let j = 0; j < 8; j++) {
-                $(`#desktop-row-${i}`).append($folderArea.clone());
+            for(let j = 0; j < 10; j++) {
+                $(`#desktop-row-${i}`).append($folderArea(i,j).clone());
             }
         }
     }
